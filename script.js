0fbe05b460ebe27cedf477eab8425013049ff550
Matheus <matheuspatrocinio258@gmail.com>
2024-01-12 18:50:57 -0300

    atualizando repositorio

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById("scrollButton").addEventListener("click", function() {
+//     // Scroll down 500 pixels in 500 milliseconds
+//     window.scrollBy({
+//         top: 500,
+//         behavior: 'smooth'
+//     });
+// });
