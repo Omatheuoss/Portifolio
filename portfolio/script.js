7dca889820fc10f35df9fd437a627fdbd1b1d16a
Matheus <matheuspatrocinio258@gmail.com>
2024-01-16 17:29:33 -0300

    novo repositorio

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+const nav = documento.querysSelector('.nav')
+const nav = documento.querysSelector('.navbar_mobile')
+const nav = documento.querysSelector('.burguer')
+
+button.addEventListener('click', function(){
+    navbar_mobile.classlist.toggle('active');
+})
--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("scrollButton").addEventListener("click", function() {
-    Scroll down 500 pixels in 500 milliseconds
-    window.scrollBy({
-        top: 500,
-        behavior: 'smooth'
-     });
-});
